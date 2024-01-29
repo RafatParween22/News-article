1b81518a2f10cd5eb377c6204bddd3ff6091789a
Rafat Parween <rafat22@navgurukul.org>
2024-01-29 10:19:25 +0530

    footer

---
+++ app/footer.js
@@ -0,0 +1,20 @@
+"use client"
+import React from 'react';
+import './globals.css';
+
+function Footer() {
+	return (
+		<>
+
+			<div class="card-body">
+				<span class="card-title">NewsAPI <img className='ellipsefooter' src="Ellipse.png"></img>Org is not responsible for the content of external sites</span>
+			</div>
+
+
+
+
+		</>
+	)
+}
+
+export default Footer;
